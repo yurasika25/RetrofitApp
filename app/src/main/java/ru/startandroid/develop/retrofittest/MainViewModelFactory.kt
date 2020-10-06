2aae45702aeb83725b8122a312428e69f5a86bc1
yurasika25 <yrasika80@gmail.com>
2020-10-06 18:00:17 +0300

    "Отримуємо дані з андроїд студії за допомогою Retrofit (01.10.2020)" нове (6.10.2020)!

--- app/src/main/java/ru/startandroid/develop/retrofittest/MainViewModelFactory.kt
+++ app/src/main/java/ru/startandroid/develop/retrofittest/MainViewModelFactory.kt
@@ -11,12 +11,3 @@ class MainViewModelFactory
         return MainViewModel(repository) as T
     }
 }
-
-// Експеримент
-class Map
-    (private val repository: Repository
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return MainViewModel(repository) as T
-    }
-}
